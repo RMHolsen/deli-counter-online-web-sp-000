1a1ba49c0f4405cc6da97e8a0da519b91a474111
Rosario Holsen <rholsen@gmail.com>
2020-08-18 14:09:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    line_order = []
+    array.each_with_index do |name, index|
+    idx = (index + 1).to_s
+    line_order << "#{idx}. #{name}"
+    end
+    puts "The line is currently: " + line_order.join(" ")
+  end
+end 
